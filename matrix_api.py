7989fc14ff91654b42e7cbd62ad935848f6ab2d1
Kyle Into <kyleatyahoo@gmail.com>
2023-03-02 21:30:44 -0500

    add 6 and q

--- matrix_api.py
+++ matrix_api.py
@@ -2,7 +2,7 @@ from rgbmatrix import graphics, RGBMatrix, RGBMatrixOptions
 from dataclasses import dataclass
 
 font = graphics.Font()
-font.LoadFont("./7x13.bdf")
+font.LoadFont("./tom-thumb.bdf")
 
 @dataclass
 class Color:
@@ -25,4 +25,22 @@ class Matrix:
 
     def drawCircle(self, x: int, y: int, radius: int, color: Color):
         graphics.DrawCircle(self.matrix, x, y, radius, graphics.Color(color.r, color.g, color.b))
+
+    def drawFilledCircle(self, x: int, y: int, radius: int, color: Color):
+        for x_diff in range(-radius, radius):
+            y_diff = radius - abs(x_diff)
+            graphics.DrawLine(
+                    self.matrix,
+                    x + x_diff,
+                    y - y_diff,
+                    x + x_diff,
+                    y + y_diff,
+                    graphics.Color(color.r, color.g, color.b)
+                )
+
+    def drawLine(self, x: int, y: int, x2: int, y2: int, color: Color):
+        graphics.DrawLine(self.matrix, x, y, x2, y2, graphics.Color(color.r, color.g, color.b))
+
+    def clear(self):
+        self.matrix.Clear()
 
--- trains.py
+++ trains.py
@@ -1,19 +1,73 @@
 #!/usr/bin/env python
-import time
+import sched, time
+from datetime import datetime, timedelta
 from train_api import train_estimates
 from matrix_api import Matrix, Color
 
-# print(train_estimates("Q", "Q03S"))
 matrix = Matrix()
+q_color = Color(255, 205, 0)
+six_color = Color(0, 147, 61)
+text_color = Color(0, 211, 0)
+no_color = Color(0, 0, 0)
+seconds: int = 0
+scheduler = sched.scheduler(time.time, time.sleep)
+q_times = None
+six_times = None
+
+def update_times():
+    global q_times, six_times
+    q_times = train_estimates("Q", "Q03S")
+    six_times = train_estimates("6", "628S")
+
+def format_times(times) -> str:
+    now = datetime.now()
+    real_trains = list(filter(lambda x: x > now + timedelta(minutes=4), times))
+    if len(real_trains) < 3:
+        print("bad. train times empty")
+    time_untils = [real_train - now for real_train in real_trains]
+    return f'{time_untils[0].seconds // 60 :02d}:{time_untils[0].seconds % 60 :02d},{",".join([str(t.seconds // 60) for t in time_untils[1:3]])}'
+
 
 def draw():
-    q_color = Color(255, 255, 0)
-    matrix.drawText(0, 10, q_color, "Test")
-    matrix.drawCircle(15, 15, 10, q_color)
+    global seconds, scheduler, q_times, six_times
+    scheduler.enter(1, 1, draw, ())
+    seconds += 1
+    if seconds == 30:
+        seconds = 0
+        scheduler.enter(30, 2, update_times, ())
+    matrix.clear()
+
+    # Draw recency line
+    matrix.drawLine(0, 0, seconds * 2, 0, q_color)
+
+    q_y = 5
+    # Draw Q
+    matrix.drawFilledCircle(4, q_y + 2, 4, q_color)
+    matrix.drawText(3, q_y + 5, no_color, "Q")
+    # Draw stop + down arrow
+    matrix.drawText(9, q_y + 5, q_color, "72")
+    matrix.drawLine(18, q_y, 18, q_y + 4, q_color)
+    matrix.drawLine(17, q_y + 3, 19, q_y + 3, q_color)
+    # Draw times
+    matrix.drawText(21, q_y + 5, text_color, format_times(q_times))
+
+    
+    six_y = 15
+    # Draw 6
+    matrix.drawFilledCircle(4, six_y + 2, 4, six_color)
+    matrix.drawText(3, six_y + 5, no_color, "6")
+    # Draw stop + down arrow
+    matrix.drawText(9, six_y + 5, six_color, "68")
+    matrix.drawLine(18, six_y, 18, six_y + 4, six_color)
+    matrix.drawLine(17, six_y + 3, 19, six_y + 3, six_color)
+    # Draw times
+    matrix.drawText(21, six_y + 5, text_color, format_times(six_times))
 
 def main():
-    draw()
-    time.sleep(5)
+    matrix.drawText(0, 10, text_color, "loading")
+    update_times()
+    scheduler.enter(1, 2, draw, ())
+    scheduler.run()
 
 if __name__ == '__main__':
     main()
